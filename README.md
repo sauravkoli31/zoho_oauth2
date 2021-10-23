22b4a4623de95aed241de5790e76cac79ed363fe
sauravkoli31 <nasaspace91@gmail.com>
2021-10-24 03:46:43 +0400

    First Commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Manage Engine - Service Desk OAuth 2.0
+
+Uses OAuth 2.0 for generating access_tokens for using Zoho APIs.
+
+
+## Generate required credentials
+
+- **CLIENT_ID | CLIENT_SECRET** - Generate the two values from [https://api-console.zoho.com/](https://api-console.zoho.com/).
+Add a self client, and copy the CLIENT_ID and CLIENT_SECRET into your .env file.
+
+## API Documentation
+
+The access tokens have only been tested with Manage Engine Service Desk although it should work with most Zoho products. 
+
+Learn more about the APIs from the link below.
+
+- [ManageEngine ServiceDesk Plus Cloud API Documentation](https://www.manageengine.com/products/service-desk/sdpod-v3-api/index.html)
+
+- [ZOHO People API Documentation](https://www.manageengine.com/products/service-desk/sdpod-v3-api/SDPOD-V3-API.html#add-request)
+
+
+## Example Usage
+
+
+    from zoho_oauth2 import ZohoAPITokens
+    if __name__ == '__main__':
+        test = ZohoAPITokens(
+            client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
+            redirect_uri=REDIRECT_URI,
+            scope=SCOPES(as string, each scope separated by comma)
+        )
+        print(test.auth_token())
---
+++ setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+setup(
+  name = 'ManageEngine_SD_OAuth2',         # How you named your package folder (MyLib)
+  packages = ['ManageEngine_SD_OAuth2'],   # Chose the same as "name"
+  version = '1.0',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Implements OAuth2.0 flow to use ManageEngine Service Desk API.',   # Give a short description about your library
+  author = 'Saurav Koli',                   # Type in your name
+  author_email = 'sauravkoli3105@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'requests',
+          'json',
+          'urllib',
+          'pickle',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+  ],
+)
