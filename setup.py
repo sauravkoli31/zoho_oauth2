836b8ba649d170c8620e0f31014f95c97d8d2f2b
sauravkoli31 <nasaspace91@gmail.com>
2021-10-24 04:19:27 +0400

    Final commit before launch

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'zoho_oauth2',         # How you named your package folder (MyLib)
   packages = ['zoho_oauth2'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
+  version = '1.0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Implements OAuth2.0 flow to use ManageEngine Service Desk API.',   # Give a short description about your library
   author = 'Saurav Koli',                   # Type in your name
   author_email = 'sauravkoli3105@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/sauravkoli31/zoho_oauth2',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/sauravkoli31/zoho_oauth2/archive/refs/tags/1.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/sauravkoli31/zoho_oauth2/archive/refs/tags/1.0.1.tar.gz',    # I explain this later on
   keywords = ['zoho', 'ZohoAPI', 'ZOHO API', 'Manage Engine'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests'
